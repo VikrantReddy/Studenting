3ec837942492dfb52dba1aff0233b9b8925b3abb
Restyled.io <commits@restyled.io>
2022-05-29 09:28:30 +0000

    Restyled by reorder-python-imports

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-# Create your views here.
 from .models import Post
+# Create your views here.
 
 
 class BlogListView(ListView):
